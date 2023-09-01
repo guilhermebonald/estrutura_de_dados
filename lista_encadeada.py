f8e217c25696a0669919b645ec6bee8e846c9ee6
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-08-31 21:05:41 -0300

    Update

--- lista_encadeada.py
+++ lista_encadeada.py
@@ -3,29 +3,40 @@ class No:
         self.valor = valor
         self.ponteiro = ponteiro
 
+    # Retorna a reprentação.
+    def __repr__(self):
+        return '%s -> %s' % (self.valor, self.ponteiro)
+
 
 class ListaEncadeada:
-    # TODO - Ponteiro da cabeça, para o inicio da lista. Endereço de memoria do primeiro Nó.
-    def __init__(self, cabeca=None):
-        self.cabeca = cabeca
+    # Ponteiro da cabeça, para o inicio da lista. Endereço de memoria do primeiro Nó.
+    def __init__(self):
+        self.cabeca = None
 
-    # TODO -  Retorna a reprentação da Lista em memoria.
+    # Retorna a reprentação.
     def __repr__(self):
-        return str(self.cabeca)
+        return "[" + str(self.cabeca) + "]"
 
-    # TODO - Inserção primeiro item da lista.
-    def inserir_inicio(self, lista, novo_valor):
-        # ? - Cria um novo "no" com o "valor" a ser armazenado
-        novo_no = No(novo_valor)
 
-        # ? Fazer o ponteiro do novo nó apontar para o atual nó cabeça da lista.
-        novo_no.ponteiro = lista.cabeca
+# Inserção primeiro item da lista.
+def insere_no_inicio(lista, novo_valor):
+    # 1) Cria um novo nó com o valor fornecido (novo_valor).
+    novo_no = No(novo_valor)
 
-        # ? Faz com que a "cabeca" referencie o novo "no"
-        lista.cabeca = novo_no
+    # 2) Faz com que o ponteiro do novo nó aponte para o nó que era a cabeça da lista anteriormente.
+    novo_no.ponteiro = lista.cabeca
+
+    # 3) Atualiza a cabeça da lista para que ela aponte para o novo nó, tornando-o o novo nó da cabeça da lista.
+    lista.cabeca = novo_no
+
+
 
 
 lista = ListaEncadeada()
-ListaEncadeada().inserir_inicio(lista=lista, novo_valor=10)
-ListaEncadeada().inserir_inicio(lista=lista, novo_valor=5)
-print(lista)
+print("Lista vazia:", lista)
+
+insere_no_inicio(lista, 5)
+print("Lista contém um único elemento:", lista)
+
+insere_no_inicio(lista, 10)
+print("Inserindo um novo elemento:", lista)

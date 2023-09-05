11c71b066e72b475ed2fffaf8e62f41fe86242de
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-09-04 21:25:50 -0300

    Updates

--- lista_encadeada.py
+++ lista_encadeada.py
@@ -5,11 +5,11 @@ class No:
 
     # Retorna a reprentação.
     def __repr__(self):
-        return '%s -> %s' % (self.valor, self.ponteiro)
+        return "%s -> %s" % (self.valor, self.ponteiro)
 
 
 class ListaEncadeada:
-    # Inicializa a cabeça da lista como vazia, indicando que a lista está vazia no início.
+    #  Inicializa a cabeça da lista como vazia, indicando que a lista está vazia no início.
     def __init__(self):
         self.cabeca = None
 
@@ -17,26 +17,56 @@ class ListaEncadeada:
     def __repr__(self):
         return "[" + str(self.cabeca) + "]"
 
+    # Metodo que informa o tamanho da lista
+    def tamanho_lista(self):
+        atual = self.cabeca
+        tamanho = 0
 
-# Inserção primeiro item da lista.
-def insere_no_inicio(lista, novo_valor):
-    # 1) Cria um novo nó com o valor fornecido (novo_valor).
-    novo_no = No(novo_valor)
+        # verifica se a lista está vazia e retorna um 0
+        if self.cabeca == None:
+            return 0
 
-    # 2) Faz com que o ponteiro do novo nó aponte para o nó que era a cabeça da lista anteriormente.
-    novo_no.ponteiro = lista.cabeca
+        # Verifica se a lista possui somente um elemento
+        if atual.conteudo != None and atual.proximo == None:
+            return 1
 
-    # 3) Atualiza a cabeça da lista para que ela aponte para o novo nó, tornando-o o novo nó da cabeça da lista.
-    lista.cabeca = novo_no
+        # Percorre a lista até o último Nó ser vazio e adicionando +1 em cada loop.
+        while atual.proximo != None:
+            tamanho = tamanho + 1
+            atual = atual.proximo
+
+        return tamanho
+
+    # Inserção primeiro item da lista.
+    def insere_no_inicio(self, lista, novo_valor):
+        # 1) Cria um novo nó com o valor fornecido (novo_valor).
+        novo_no = No(novo_valor)
+
+        # 2) Faz com que o ponteiro do novo nó aponte para o nó que era a cabeça da lista anteriormente.
+        novo_no.ponteiro = lista.cabeca
+
+        # 3) Atualiza a cabeça da lista para que ela aponte para o novo nó, tornando-o o novo nó da cabeça da lista.
+        lista.cabeca = novo_no
 
 
+def lista_vazia(lista):
+    if lista.cabeca == None:
+        return True
+    else:
+        return False
 
 
+# Execução da Lista
 lista = ListaEncadeada()
-print("Lista vazia:", lista)
 
-insere_no_inicio(lista, 5)
+print(lista_vazia(lista))
+
+lista.insere_no_inicio(lista, 5)
 print("Lista contém um único elemento:", lista)
 
-insere_no_inicio(lista, 10)
+lista.insere_no_inicio(lista, 10)
 print("Inserindo um novo elemento:", lista)
+
+print(lista.tamanho_lista())
+
+print(lista_vazia(lista))

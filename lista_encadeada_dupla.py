251d8799890ad5ec0aaa907157395b29f53c6245
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-09-14 20:42:53 -0300

    DoubleLinkedList

---
+++ lista_encadeada_dupla.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+    
+    def display_forward(self):
+        current = self.head
+        while current:
+            print(current.data, end=" <-> ")
+            current = current.next
+        print("None")
+
+    def display_backward(self):
+        current = self.tail
+        while current:
+            print(current.data, end=" <-> ")
+            current = current.prev
+        print("None")
+
+
+# Execução
+dll = DoubleLinkedList()
+dll.append(1)
+dll.append(2)
+dll.append(3)
+
+dll.display_backward()
+dll.display_forward()

a7a91b2d2152ef7b230bb081858482da5de90a9f
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-10-05 19:51:08 -0300

    Ordenação

---
+++ desafios/algoritmo_de_ordenacao.py
@@ -0,0 +1,19 @@
+# Ordenação por Inserção
+
+def insertion_sort(lista, N):
+    mudancas = 0
+    for i in range(1, N):
+        escolhido = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j] > escolhido:
+            lista[j + 1] = lista[j]
+            j = j - 1
+            mudancas += 1
+        lista[j + 1] = escolhido
+    return mudancas
+
+notas = [11, 4, 12, 45, 2]
+
+mudancas = insertion_sort(notas, len(notas))
+
+print(notas, mudancas)

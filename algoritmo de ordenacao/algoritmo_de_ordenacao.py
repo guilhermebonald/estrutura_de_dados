1f1b89218212c437737b70f615e9a4bbbd61680e
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-10-07 11:19:35 -0300

    Organization

--- algoritmo de ordenacao/algoritmo_de_ordenacao.py
+++ algoritmo de ordenacao/algoritmo_de_ordenacao.py
@@ -2,6 +2,7 @@
 
 def insertion_sort(lista, N):
     mudancas = 0
+    # Começando do segundo elemento, pois sempre irá comparar aos numeros anteriores.
     for i in range(1, N):
         escolhido = lista[i]
         j = i - 1
@@ -13,7 +14,5 @@ def insertion_sort(lista, N):
     return mudancas
 
 notas = [11, 4, 12, 45, 2]
-
 mudancas = insertion_sort(notas, len(notas))
-
 print(notas, mudancas)
